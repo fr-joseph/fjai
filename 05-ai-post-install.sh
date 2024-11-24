0325e9dfd6e52a50a5e4cc4e221cc500fcca40f2
fj <fj@myr>
2024-11-24 16:56:58 -0700

    update packages

--- 05-ai-post-install.sh
+++ 05-ai-post-install.sh
@@ -6,47 +6,53 @@ if [ "$(whoami)" = "root" ]; then
 fi
 
 # minimal
-BASE="base base-devel cryptsetup dhcpcd git linux linux-lts mkinitcpio nss-mdns"
+BASE="base base-devel cryptsetup dhcpcd git linux linux-lts mkinitcpio nss-mdns stow"
 FIRMWARE="amd-ucode linux-firmware"
 
 # core
-TERMINAL="bat btop fd fzf just man-db openssh plocate ripgrep shellcheck tealdeer"
+GO="go gopls" # `go-task` for alternative to `just`, `go-tools` for static analysis
+TERMINAL="bat btop cloc fd fish fzf just man-db openssh plocate ripgrep shellcheck tealdeer"
 MAIL="isync msmtp notmuch"
 SECURITY="age pwgen"
 
 # desktop
 AUDIO="pipewire pipewire-jack pulseaudio pavucontrol wireplumber"
-DESKTOP="foot polkit"
+DESKTOP="foot emacs-wayland polkit"
+DEV="apache jdk-openjdk mariadb"
 FONT="noto-fonts noto-fonts-cjk noto-fonts-emoji otf-font-awesome ttf-firacode-nerd ttf-font-awesome ttf-nerd-fonts-symbols-mono"
 GPU_DRIVER="amdvlk vulkan-tools"
 MEDIA="yt-dlp mpv"
 WAYLAND="grim slurp mako bemenu-wayland sway swaybg sway-contrib swayidle swayimg swaylock xdg-desktop-portal-gtk xdg-desktop-portal-wlr xorg-xwayland"
-WEB="apache mariadb"
 
+# shellcheck disable=SC2086
 sudo pacman -S --needed \
   $BASE \
   $FIRMWARE \
+  $GO \
   $TERMINAL \
   $MAIL \
   $SECURITY \
   $AUDIO \
   $DESKTOP \
+  $DEV \
   $FONT \
   $GPU_DRIVER \
   $MEDIA \
-  $WAYLAND \
-  $WEB
+  $WAYLAND
+
+# gimp-git \
 
 if [ -f "$(which paru)" ]; then
   paru -S --needed \
-    brave-bin \
-    gimp-git \
-    passage-git
+       brave-bin \
+       exercism-bin \
+			 pandoc-bin \
+       passage-git
 else
   sudo pacman -S --needed base-devel
-  cd /tmp
+  cd /tmp || exit 1
   git clone https://aur.archlinux.org/paru.git
-  cd paru
+  cd paru || exit 1
   makepkg -si
 fi
 
@@ -54,4 +60,3 @@ fi
 # sudo systemctl enable --now sshd.service
 
 sudo pacman -Syy
-
