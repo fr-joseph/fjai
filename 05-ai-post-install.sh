0f7c4b9db675b272cac34543911d150693b0a244
fj <fj@myr>
2024-11-27 02:42:47 -0700

    [automated backup]

--- 05-ai-post-install.sh
+++ 05-ai-post-install.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
 if [ "$(whoami)" = "root" ]; then
-  echo "ERROR: cannot be root"
-  exit 1
+	echo "ERROR: cannot be root"
+	exit 1
 fi
 
 # minimal
 BASE="base base-devel cryptsetup dhcpcd git linux linux-lts mkinitcpio nss-mdns stow"
 FIRMWARE="amd-ucode linux-firmware"
 
-# core
-CLJ="clojure"
 GO="go gopls" # `go-task` for alternative to `just`, `go-tools` for static analysis
 TERMINAL="bat btop cloc fd fish fzf just man-db openssh plocate ripgrep shellcheck tealdeer"
 MAIL="isync msmtp notmuch"
@@ -18,7 +16,7 @@ SECURITY="age pwgen"
 
 # desktop
 AUDIO="pipewire pipewire-jack pulseaudio pavucontrol wireplumber"
-DESKTOP="foot emacs-wayland polkit"
+DESKTOP="foot polkit"
 DEV="apache jdk-openjdk mariadb"
 FONT="noto-fonts noto-fonts-cjk noto-fonts-emoji otf-font-awesome ttf-firacode-nerd ttf-font-awesome ttf-nerd-fonts-symbols-mono"
 GPU_DRIVER="amdvlk vulkan-tools"
@@ -27,37 +25,40 @@ WAYLAND="grim slurp mako bemenu-wayland sway swaybg sway-contrib swayidle swayim
 
 # shellcheck disable=SC2086
 sudo pacman -S --needed \
-  $BASE \
-  $FIRMWARE \
-	$CLJ \
-  $GO \
-  $TERMINAL \
-  $MAIL \
-  $SECURITY \
-  $AUDIO \
-  $DESKTOP \
-  $DEV \
-  $FONT \
-  $GPU_DRIVER \
-  $MEDIA \
-  $WAYLAND
+	$BASE \
+	$FIRMWARE \
+	$GO \
+	$TERMINAL \
+	$MAIL \
+	$SECURITY \
+	$AUDIO \
+	$DESKTOP \
+	$DEV \
+	$FONT \
+	$GPU_DRIVER \
+	$MEDIA \
+	$WAYLAND
 
 # gimp-git \
 
 if [ -f "$(which paru)" ]; then
-  paru -S --needed \
-       brave-bin \
-			 babashka-bin \
-			 clojure-lsp-bin \
-       exercism-bin \
-			 pandoc-bin \
-       passage-git
+	paru -S --needed \
+			brave-bin \
+			exercism-bin \
+			pandoc-bin
+
+	# it asks to review package build even if no updates are needed
+	# so just don't even ask me...
+	if ! [ -f "$(which passage)" ]; then
+			paru -S --needed passage-git
+	fi
+
 else
-  sudo pacman -S --needed base-devel
-  cd /tmp || exit 1
-  git clone https://aur.archlinux.org/paru.git
-  cd paru || exit 1
-  makepkg -si
+	sudo pacman -S --needed base-devel
+	cd /tmp || exit 1
+	git clone https://aur.archlinux.org/paru.git
+	cd paru || exit 1
+	makepkg -si
 fi
 
 # sudo systemctl enable --now avahi-daemon.service
