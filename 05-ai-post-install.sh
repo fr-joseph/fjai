4edab089ad9fa85f6829a26b2ec08dfa5f737b4e
fj <fj@myr>
2024-12-05 02:41:55 -0700

    [automated backup]

--- 05-ai-post-install.sh
+++ 05-ai-post-install.sh
@@ -9,7 +9,7 @@ fi
 BASE="base base-devel cryptsetup dhcpcd expac git git-lfs linux linux-lts mkinitcpio nss-mdns rlwrap stow tmux"
 FIRMWARE="amd-ucode linux-firmware"
 
-TERMINAL="bat btop cloc eza fd fish fzf just man-db openssh plocate ripgrep shellcheck tealdeer zoxide"
+TERMINAL="alot bat btop cloc eza fd fish fzf just man-db openssh plocate ripgrep shellcheck tealdeer zoxide w3m"
 MAIL="isync msmtp notmuch"
 SECURITY="age pwgen"
 
